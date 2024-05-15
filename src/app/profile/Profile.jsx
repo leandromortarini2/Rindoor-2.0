34d15352a93fe37813a033fd45267fd882cc8d88
leandro mortarini <leandromortarini2@gmail.com>
2024-05-15 01:03:10 -0300

    REGISTER PARTE3

--- src/app/profile/Profile.jsx
+++ src/app/profile/Profile.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import { Register } from "../../components/Register/Register";
 import { useSession } from "next-auth/react";
 
@@ -10,25 +11,24 @@ function ProfileClient() {
   return (
     session && (
       <>
-        <div className="w-full min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-yellow-300 via-yellow-100 to-yellow-300 h-screen">
-          <h2 className="text-gray-900 text-5xl font-bold">Personal Profile</h2>
-          <div className="w-1/2 h-80 mt-10 bg-gray-900 rounded-3xl flex justify-evenly items-center shadow-black shadow-2xl">
-            <img
-              src={session.user.image}
-              className="w-40 h-40 rounded-full border-yellow-500 border-4"
-            />
-
-            <div>
-              <h2 className="text-yellow-500 text-5xl font-bold">
+        <div className="w-full min-h-screen flex flex-col justify-start items-center bg-gradient-to-r from-yellow-300 via-yellow-100 to-yellow-300">
+          <div className="w-1/2 h-80 mt-10 mb-5 flex flex-col  justify-evenly items-center bg-gray-900 rounded-2xl ">
+            {" "}
+            <div className="w-3/4 flex flex-col items-center ">
+              <img
+                src={session.user.image}
+                className="w-40 h-40 rounded-full border-gray-900 border-4"
+              />
+              <h2 className="text-yellow-500  text-5xl font-bold">
                 {session.user.name}
               </h2>
               <p className="text-white text-2xl font-semibold">
                 {session.user.email}
               </p>
             </div>
-          </div>
-        </div>
-        <Register />
+          </div>{" "}
+          <Register />{" "}
+        </div>{" "}
       </>
     )
   );
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,18 +1,51 @@
 "use client";
 import React from "react";
+import { useSession } from "next-auth/react";
 
 export const Register = () => {
+  const { data: session } = useSession();
   return (
-    <div className="w-full bg-gray-800 flex flex-col items-center">
-      <h2 className="text-5xl text-center">Register</h2>
-      <form className="w-3/4 h-[450px] flex flex-col justify-evenly items-center ">
-        <input type="text" placeholder="firstName..." />
-        <input type="text" placeholder="lastName..." />
-        <input type="email" placeholder="email..." />
-        <input type="text" placeholder="celu..." />
-        <input type="text" placeholder="address..." />
+    <div className="w-1/2 min-h-screen  flex flex-col items-center">
+      <form className="w-3/4 h-[450px] flex flex-col justify-evenly items-center bg-gray-900 rounded-3xl">
+        <h2 className="text-2xl text-center text-yellow-500 font-bold  ">
+          Complete your personal information
+        </h2>
+        <div className="w-full flex flex-col justify-center items-center">
+          <label htmlFor="">Name</label>
+          <input
+            type="text"
+            value={session.user.name}
+            className=" text-gray-900 font-semibold w-1/2"
+          />
+        </div>
+        <div className="w-full flex flex-col justify-center items-center">
+          <label htmlFor="">Email</label>
+          <input
+            type="email"
+            value={session.user.email}
+            className=" text-gray-900 font-semibold w-1/2"
+          />
+        </div>
+        <div className="w-full flex flex-col justify-center items-center">
+          <label htmlFor="">Phone</label>
+          <input
+            type="text"
+            placeholder="Phone..."
+            className=" text-gray-900 font-semibold w-1/2"
+          />
+        </div>
+
+        <div className="w-full flex flex-col justify-center items-center">
+          <label htmlFor="">Address</label>
+          <input
+            type="text"
+            placeholder="Address..."
+            className=" text-gray-900 font-semibold w-1/2"
+          />{" "}
+        </div>
+
         <select name="role" id="" className="bg-gray-900">
-          <option value="client">PPPP</option>
+          <option value="client">Client</option>
           <option value="professional">professional</option>
         </select>
         <button className="bg-yellow-900">Register</button>
