83174ce21153def105525b0f14472a466b6b9f2d
Agustina <agustinaayelensosa@gmail.com>
2024-05-19 15:23:39 -0300

    suscripciones funcionando con id de usuario

--- src/helpers/helperSubscription.js
+++ src/helpers/helperSubscription.js
@@ -4,7 +4,7 @@ import React from 'react'
 export const getSubscription = async () => {
     try {
         const response= await axios.get('https://rindoor-backend.onrender.com/subscriptions/plans')
-        console.log(response.data)
+        // console.log(response.data)
         return response.data
     } catch (error) {
         console.log(error)
@@ -13,20 +13,31 @@ export const getSubscription = async () => {
 
 export const postSubscription = async (plan) => {
     try {
+        console.log('plan helper', plan)
         const response = await axios.post('https://rindoor-backend.onrender.com/subscriptions', plan )
-        console.log(response.data)
+        console.log('response post', response.data)
         return response.data
     } catch (error) {
         console.log(error)        
     }
 }
 
-export const getUser = async (userId) => {
+export const getUser = async (id) => {
     try {
-        const response = await axios.get(`https://rindoor-backend.onrender.com/subscriptions/user/${userId}`)
+        const response = await axios.get(`https://rindoor-backend.onrender.com/subscriptions/${id}`)
         console.log(response.data)
         return response.data
     } catch (error) {
         console.log(error)         
+    }
+}
+
+export const cancellSubscription = async (id) => {
+    try {
+        const response = await axios.put(`https://rindoor-backend.onrender.com/subscriptions/cancel/${id}`)
+        console.log('response helper cancelled', response)
+        return response
+    } catch (error) {
+        console.log(error)    
     }
 }
