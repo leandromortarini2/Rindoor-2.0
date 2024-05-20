d335df844e0d5a3087917de59cc36852ed4149b0
RivarolaFG <fer.rivarolag@gmail.com>
2024-05-20 14:26:04 -0300

    nuevo commit

--- src/components/WorkCard/WorkCard.jsx
+++ src/components/WorkCard/WorkCard.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export const WorkCard = ({ card }) => {
   return (
     <Link
-      href={`/works/${card.id}`}
+      href={`/works`}
       key={card.name}
       className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
     >
