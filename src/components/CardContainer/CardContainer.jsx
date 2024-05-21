40b48963e9aeade79d8c9329758cd6ae0058e7fb
RivarolaFG <fer.rivarolag@gmail.com>
2024-05-21 18:30:50 -0300

    pagina /works casi final

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -5,12 +5,9 @@ export const CardContainer = ({ worksData }) => {
   return (
     <div className="w-full bg-opacity-20 max-h-full overflow-y-auto">
       <div className="grid md:grid-cols-2 grid-cols-1 gap-2 py-5 px-5">
-        <div className="solo para prueba wea">
-          <h1> HOLA MUNDO</h1>
-          {worksData.map((card, index) => (
-            <div key={index}> {card.created_at} </div>
-          ))}
-        </div>
+        {worksData.map((card) => (
+          <WorkCard key={card.id} card={card} />
+        ))}
       </div>
     </div>
   );
