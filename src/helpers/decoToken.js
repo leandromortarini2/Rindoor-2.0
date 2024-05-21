6dbefd7cda0405d99b073d13f49cff299ae54dcb
leandro mortarini <leandromortarini2@gmail.com>
2024-05-21 11:02:22 -0300

    documentacion de servicios Leandro

--- src/helpers/decoToken.js
+++ src/helpers/decoToken.js
@@ -1,3 +1,9 @@
+/**
+ * Esta funcion decodifica el token y devuelve los datos del usuario.
+ * @param {string} token  Recibe un token
+ * @returns {object}  devuelve los datos del usuario
+ */
+
 export function decoToken(token) {
   const arrayToken = token.split(".");
   const tokenPayload = JSON.parse(atob(arrayToken[1]));
