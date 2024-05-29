c5ccd2cc9edac1ce4a773e8cbf74e4ad1b5f0b42
RivarolaFG <fer.rivarolag@gmail.com>
2024-05-29 11:49:29 -0300

    pagina detalles work al 99,5%

--- src/components/WorkPageCard/WorkPageCard.jsx
+++ src/components/WorkPageCard/WorkPageCard.jsx
@@ -189,7 +189,7 @@ export const WorkPageCard = ({ cardData }) => {
                 <div className="md:col-span-5 text-right">
                   <div className="inline-flex items-end">
                     <button
-                      onClick={handleClick1}
+                      onClick={handleClick}
                       className="bg-yellow-300 text-gray-900 hover:bg-gray-700 hover:text-yellow-500 font-bold py-2 px-4 rounded"
                     >
                       Enviar!
