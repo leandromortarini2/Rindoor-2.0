6370eb1774bcbfc414fc14b32c3ad70ee5b2922a
RivarolaFG <fer.rivarolag@gmail.com>
2024-05-29 22:18:30 -0300

    final proteccion de worksID

--- src/app/works/[worksId]/page.js
+++ src/app/works/[worksId]/page.js
@@ -3,9 +3,35 @@ import { WorkPageCard } from "../../../components/WorkPageCard/WorkPageCard";
 import React, { useEffect, useState } from "react";
 import { getWorkById } from "../../../helpers/helperWorksPage";
 import Loader from "../../../components/Loader/Loader";
+import { useRouter } from "next/navigation";
+import { useAuth } from "../../context/Context";
+import Swal from "sweetalert2";
+import { useSession } from "next-auth/react";
 export default function page({ params }) {
+  const { data: session } = useSession();
   const [cardData, setCardData] = useState({});
   const [LoaderState, setLoaderState] = useState(true);
+  const router = useRouter();
+  useEffect(() => {
+    console.log("rtarta", session);
+  }, [session]);
+  useEffect(() => {
+    if (!session) {
+      if (session === null) {
+        Swal.fire({
+          title: "Espera!",
+          text: "Para ver los datos del trabajo, debes iniciar sesion!",
+          icon: "info",
+          confirmButtonText: "niciar sesion",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            router.push("/");
+          }
+        });
+      }
+    }
+  }, [session]);
+
   useEffect(() => {
     getWorkById(params.worksId)
       .then((responseData) => {
